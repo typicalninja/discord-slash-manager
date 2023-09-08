892f9348b8a0c228f83e270d576110c40d1ccba1
typicalninja493 <65993466+typicalninja493@users.noreply.github.com>
2023-09-08 21:13:27 +0530

    fix: svelte config path

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-            base: dev ? '' : 'discord-slash-manager',
+            base: dev ? '' : '/discord-slash-manager',
         },
 		alias: {
 			$lib: path.resolve('./src/lib'),
